0d003ee683294135f86ee5450ea331740ee7b427
andrey_mazo <andreymazo@mail.ru>
2023-05-16 19:49:27 +0300

    добавил кнопку Refresh Table

--- spa_table/views.py
+++ spa_table/views.py
@@ -1,10 +1,13 @@
 import names
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import QuerySet
+from django.http import HttpResponseRedirect
 from django_tables2 import SingleTableView
 from spa_table.models import Values_tableTable, Values_table
 from random import randint
 
 
-class TableListView(SingleTableView):
+class TableListView(SingleTableView):  #
     model = Values_table
     table_class = Values_tableTable
     for i in range(1, 10):
@@ -14,12 +17,63 @@ class TableListView(SingleTableView):
             distance=randint(1, 100),
         )
         values.save()
+    print('___________2_____________')
 
     queryset = Values_table.objects.all()
     template_name = "spa_table/Values_table_list.html"
     ordering = ('distance',)  # quantity, name
     table_pagination = {"per_page": 5}
+    def get_queryset(self, **kwargs):
+        """
+        Return the list of items for this view.
 
-# {% load django_tables2 %}
-# {% render_table table %}
+        The return value must be an iterable and may be an instance of
+        `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+        """
+
+
+        global queryset
+        if self.request.method == "GET":
+            # for i in range(1, 10):
+            #     values = Values_table.objects.create(
+            #         name=names.get_last_name(),
+            #         quantity=randint(1, 100),
+            #         distance=randint(1, 100),)
+            #     values.save()
+                if self.queryset is not None:
+                    queryset = self.queryset
+                    if isinstance(queryset, QuerySet):
+                        queryset = queryset.all()
+                if self.model is not None:
+                    queryset = self.model._default_manager.all()
+                else:
+                    raise ImproperlyConfigured(
+                        "%(cls)s is missing a QuerySet. Define "
+                        "%(cls)s.model, %(cls)s.queryset, or override "
+                        "%(cls)s.get_queryset()." % {"cls": self.__class__.__name__}
+                    )
+                ordering = self.get_ordering()
+                if ordering:
+                    if isinstance(ordering, str):
+                        ordering = (ordering,)
+                    queryset = queryset.order_by(*ordering)
+        Values_table.objects.all().delete()
+
+        for i in range(1, 10):
+
+            values = Values_table.objects.create(
+                name=names.get_last_name(),
+                quantity=randint(1, 100),
+                distance=randint(1, 100), )
+            values.save()
+        queryset = Values_table.objects.all()
+
+        return queryset
+
+
+    # print('___________1_____________')
+
+
+    # print('___________2_____________')
+
 # https://faint-adasaurus-4bc.notion.site/web-Python-adf33211e9cc4d6b9ec2c0c619ecab31
